786609d17438c269a76b8bcdb5ac07e7b6d7cc55
apo5tol <alexkuzhel.biz@gmail.com>
2021-02-09 22:40:52 +0300

    feat(rgb_utils): add rgb tools

--- rgb_utils.py
+++ rgb_utils.py
@@ -1,5 +1,8 @@
-{
-    "2": [
+from typing import List, Dict
+from pprint import pprint
+
+PACKAGES_DATA: Dict[str, List[str]] = {
+    "init": [
         "071f0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
         "071f0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
         "07050000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
@@ -34,12 +37,62 @@
         "07030605000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
         "07030601000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
         "07030601000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
-        "07030607000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
-        "07030608000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
-        "070306090000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
-        "0703060a0000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff00000000000000000000",
-        "0703060b000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
-        "0703060c000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
+    ],
+    "final": [
         "07050000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
-    ]
+    ],
 }
+
+RGB_PACKAGES_PATTERNS = {
+    "R": ["0703060b0000{}", "0703060c0000{}"],
+    "G": ["070306090000{}", "0703060a0000{}"],
+    "B": ["070306070000{}", "070306080000{}"],
+}
+
+
+class HexColor:
+    hex_color: str
+
+    def __init__(self, hex_color: str = "000000"):
+        self.hex_color = hex_color
+
+    @property
+    def R(self) -> str:
+        return self.hex_color[:2]
+
+    @property
+    def G(self) -> str:
+        return self.hex_color[2:4]
+
+    @property
+    def B(self) -> str:
+        return self.hex_color[4:6]
+
+    def __getitem__(self, key: str) -> str:
+        value = getattr(self, key, None)
+        if value is None:
+            raise KeyError
+        return value
+
+
+class RGBProfile:
+    hex_values: HexColor
+    packages: List[str]
+    order_rgb_keys = ("B", "G", "R")
+
+    def __init__(self, hex_color: HexColor):
+        self.hex_color = hex_color
+        self.packages_data = []
+        self.__prepare_packages()
+
+    def __return_rgb_packages(self, key: str) -> List[str]:
+        rgb_packages = []
+        for pattern in RGB_PACKAGES_PATTERNS[key]:
+            rgb_packages.append(pattern.format(self.hex_color[key] * 58))
+        return rgb_packages
+
+    def __prepare_packages(self) -> None:
+        self.packages_data = PACKAGES_DATA["init"]
+        for rgb_key in self.order_rgb_keys:
+            self.packages_data += self.__return_rgb_packages(rgb_key)
+        self.packages_data += PACKAGES_DATA["final"]

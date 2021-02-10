76314e5bd328cfe20546efc6a0b0a87f99347957
apo5tol <alexkuzhel.biz@gmail.com>
2021-02-10 21:59:14 +0300

    feat(main): add args

--- main.py
+++ main.py
@@ -1,12 +1,40 @@
+import argparse
+import functools
+
 from device import Keyboard
 from rgb_utils import HexColor, RGBProfile
 
 ID_VENDOR: int = 0x09DA
 ID_PRODUCT: int = 0xFA10
 
+hex_int = functools.partial(int, base=0)
 
 if __name__ == "__main__":
-    rgb_keyboard = Keyboard(ID_VENDOR, ID_PRODUCT)
-    color = HexColor("00ff00")
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-c",
+        "--color",
+        type=str,
+        help="color in hex format. example: 00ff00",
+        required=True,
+    )
+    parser.add_argument(
+        "-id_v",
+        "--id_vendor",
+        type=hex_int,
+        help="vendor id in hex format, default: 0x09DA".format(ID_VENDOR),
+        default=ID_VENDOR,
+    )
+    parser.add_argument(
+        "-id_p",
+        "--id_product",
+        type=hex_int,
+        help="product id in hex format, default: 0xFA10".format(ID_PRODUCT),
+        default=ID_PRODUCT,
+    )
+    args = parser.parse_args()
+
+    rgb_keyboard = Keyboard(args.id_vendor, args.id_product)
+    color = HexColor(args.color)
     rgb_profile = RGBProfile(color)
     rgb_keyboard.set_rgb_highlight(rgb_profile)
